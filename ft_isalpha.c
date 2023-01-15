2c69765f7bfc5dba9c450371729bc51e89e2d4b9
jmehlig <104910768+jmehlig@users.noreply.github.com>
2023-01-15 14:29:03 +0100

    Update ft_isalpha.c

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -12,14 +12,13 @@
 
 #include "libft.h"
 
+//returns 1 if the given int is a letter of the alphabet in ascii
+//returns 0 otherwise
+
 int	ft_isalpha(int c)
 {
 	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
