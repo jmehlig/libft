3a0bfa3dce4854e808b57d71eca5df2da5e08b35
jmehlig <104910768+jmehlig@users.noreply.github.com>
2023-01-15 14:20:20 +0100

    Update ft_f_greater_f.c

--- ft_f_greater_f.c
+++ ft_f_greater_f.c
@@ -12,11 +12,13 @@
 
 #include "libft.h"
 
+//returns 1 if f1 is actually (with respect to a possible small aberration) greater than f2
+
 int	ft_f_greater_f(float f1, float f2)
 {
 	float	precision;
 
-	precision = 0.00001;
+	precision = 0.001;
 	if (((f1 - precision) > f2))
 		return (1);
 	else
