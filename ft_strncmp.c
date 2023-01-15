dd55d2a418779df18e780f0f8bab622c92aeeab0
jmehlig <104910768+jmehlig@users.noreply.github.com>
2023-01-15 16:35:07 +0100

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -12,6 +12,9 @@
 
 #include "libft.h"
 
+//compares the first n characters of the strings s1 and s2
+// returns difference in the first different char or 0 for equality
+
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
@@ -20,13 +23,9 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (i < n)
 	{
 		if (s1[i] != s2[i] || (s1[i] == 0 || s2[i] == 0))
-		{
 			return ((int)((unsigned char)s1[i] - (unsigned char)s2[i]));
-		}
 		else
-		{
 			i++;
-		}
 	}
 	return (0);
 }
