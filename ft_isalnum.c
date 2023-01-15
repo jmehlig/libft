4e54b478a1d0d6702b901bc994ab3dee90a5132f
jmehlig <104910768+jmehlig@users.noreply.github.com>
2023-01-15 14:27:46 +0100

    Update ft_isalnum.c

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -12,14 +12,13 @@
 
 #include "libft.h"
 
+//returns 1 if the given int is alphanumerical
+//otherwise 0 is returned
+
 int	ft_isalnum(int c)
 {
 	if (ft_isalpha(c) == 1 || ft_isdigit(c) == 1)
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
