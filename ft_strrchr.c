bd006ec0ee0093e2bfdd6e6d667b734a18425329
jmehlig <104910768+jmehlig@users.noreply.github.com>
2023-01-15 16:40:08 +0100

    Update ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -12,21 +12,20 @@
 
 #include "libft.h"
 
+//searches for last occurence of c in the string s
+//if non-existent return 0
+
 char	*ft_strrchr(const char *s, int c)
 {
 	size_t	i;
 
 	i = ft_strlen(s);
 	if (c == 0)
-	{
 		return ((char *) &s[i]);
-	}
 	while (i != 0)
 	{
 		if (s[i - 1] == c)
-		{
 			return ((char *) &s[i - 1]);
-		}
 		i--;
 	}
 	return (0);
